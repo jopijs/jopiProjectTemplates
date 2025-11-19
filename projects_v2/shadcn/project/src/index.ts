98ba033ac843dcc666736682aae69c7e0eb9dbcb
Johan PIQUET <johan.piquet@gmail.com>
2025-11-19 21:50:59 +0100

    Improved templates

--- projects_v2/shadcn/project/src/index.ts
+++ projects_v2/shadcn/project/src/index.ts
@@ -2,21 +2,4 @@ import {jopiApp} from "jopi-rewrite";
 
 jopiApp.startApp(import.meta, jopiEasy => {
     jopiEasy.create_creatWebSiteServer()
-
-        .fastConfigure_cors(["http://mywebsiteA", "http://mywebsiteB"])
-
-        .fastConfigure_jwtTokenAuth("key", async (info: any) => {
-            if (info.email == "allowed@domain.com") {
-                return {
-                    isOk: true,
-
-                    userInfos: {
-                        id: "user1",
-                        roles: ["admin", "writer"]
-                    }
-                }
-            }
-
-            return {isOk: false};
-        })
 });
--- projects_v2/shadcn/project/src/mod_sample/uiInit.tsx
+++ projects_v2/shadcn/project/src/mod_sample/uiInit.tsx
@@ -1,39 +1,11 @@
 import {UiKitModule, MenuName} from "jopi-rewrite/uikit";
-import {isBrowser} from "jopi-toolkit/jk_what";
-import testHelloEvent from "@/events/test.hello";
-
 import {AudioWaveform, Command, Frame, GalleryVerticalEnd, SquareTerminal} from "lucide-react";
 
 // Note: the default class received is "ModuleInitContext"
 // but ui-kit overrides the creation step to provide an instance of UiKitModule.
 //
 export default function(myModule: UiKitModule) {
-    if (isBrowser) {
-        myModule.events.enableEventSpying((name, e) => {
-            //console.log(`Event spy - ${name}`, e);
-        });
-    }
-
-    testHelloEvent.send({});
-
-    myModule.addUiInitializer(() => {
-        console.log('Module A - UI initialized (Default)');
-    });
-
     const menuManager = myModule.getMenuManager();
-
-    menuManager.addMenuBuilder(MenuName.LEFT_MENU, (leftMenu) => {
-        // The menu builders are called each time user roles are updated.
-        // Which includes login/logout.
-        //
-        myModule.ifUserHasRoles(["admin"], () => {
-            leftMenu.selectItem(["My roles", "Role Admin"]).value = {url: "/role/admin"};
-        });
-
-        myModule.ifUserHasRoles(["writer"], () => {
-            leftMenu.selectItem(["My roles", "Role Writer"]).value = {url: "/role/writer"};
-        });
-    });
 
     menuManager.addMenuBuilder("favorites", (projectsMenu) => {
         projectsMenu.append({key: "Home", url: "/", icon: Frame});
@@ -54,11 +26,5 @@ export default function(myModule: UiKitModule) {
                 {key: "Tests", url: "/features/tests"}
             ]
         });
-    });
-
-    myModule.events.addListener("user.infosUpdated", () => {
-        myModule.ifUserHasRoles(["admin", "writer"], () => {
-            // alert("has the roles [\"admin\", \"writer\"]")
-        })
     });
 }
